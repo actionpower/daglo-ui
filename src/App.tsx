7f15f103c03efa347b78d572caf43cfeab1deeab
Yongmin Jeon <75535651+J-Ymini@users.noreply.github.com>
2024-06-03 17:33:01 +0900

    Input 컴포넌트 추가 (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AlertDialogDemo from './exampleComponents/AlertDialogDemo';
 import CheckboxDemo from './exampleComponents/CheckboxDemo';
 import DisplayContainer from './exampleComponents/DisplayContainer';
+import InputDemo from './exampleComponents/InputDemo';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       </DisplayContainer>
       <DisplayContainer title="Checkbox Demo">
         <CheckboxDemo />
+      </DisplayContainer>
+      <DisplayContainer title="Input Demo">
+        <InputDemo />
       </DisplayContainer>
     </div>
   );
