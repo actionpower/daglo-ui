8cb509af0bab53670c2b41150e12766e960cd4c8
Yongmin Jeon <75535651+J-Ymini@users.noreply.github.com>
2024-06-03 18:21:43 +0900

    Select 컴포넌트 추가 (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import AlertDialogDemo from './exampleComponents/AlertDialogDemo';
 import CheckboxDemo from './exampleComponents/CheckboxDemo';
 import DisplayContainer from './exampleComponents/DisplayContainer';
 import InputDemo from './exampleComponents/InputDemo';
+import SelectDemo from './exampleComponents/SelectDemo';
 import TextareaDemo from './exampleComponents/TextareaDemo';
 
 function App() {
   return (
-    <div className="p-4">
+    <div className="p-4 max-w-[31.25rem] bg-gray-100">
       <DisplayContainer title="Alert Dialog Demo">
         <AlertDialogDemo />
       </DisplayContainer>
@@ -18,6 +19,9 @@ function App() {
       </DisplayContainer>
       <DisplayContainer title="Textarea Demo">
         <TextareaDemo />
+      </DisplayContainer>
+      <DisplayContainer title="Select Demo">
+        <SelectDemo />
       </DisplayContainer>
     </div>
   );
