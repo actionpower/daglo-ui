13f84533a90739486cc2d363234d94ac75423b19
Yongmin Jeon <75535651+J-Ymini@users.noreply.github.com>
2024-06-04 16:26:15 +0900

    Radio group 컴포넌트 추가 (#15)

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import DialogDemo from './exampleComponents/DialogDemo';
 import DisplayContainer from './exampleComponents/DisplayContainer';
 import InputDemo from './exampleComponents/InputDemo';
 import PaginationDemo from './exampleComponents/PaginationDemo';
+import RadioGroupDemo from './exampleComponents/RadioGroupDemo';
 import SelectDemo from './exampleComponents/SelectDemo';
 import SonnerDemo from './exampleComponents/SonnerDemo';
 import SwitchDemo from './exampleComponents/SwitchDemo';
@@ -54,14 +55,17 @@ function App() {
       <DisplayContainer title="Alert Demo">
         <AlertDemo />
       </DisplayContainer>
-      <DisplayContainer title="Switch Demo">
-        <SwitchDemo />
-      </DisplayContainer>
       <DisplayContainer title="Pagination Demo">
         <PaginationDemo />
       </DisplayContainer>
+      <DisplayContainer title="Switch Demo">
+        <SwitchDemo />
+      </DisplayContainer>
       <DisplayContainer title="Button Demo">
         <ButtonDemo />
+      </DisplayContainer>
+      <DisplayContainer title="RadioGroup Demo">
+        <RadioGroupDemo />
       </DisplayContainer>
     </div>
   );
