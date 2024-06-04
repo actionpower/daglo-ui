6a298d9313c435daa8f4057c58e11bca47e82a94
yongmin.jeon <yongmin.jeon@actionpower.kr>
2024-06-04 15:48:00 +0900

    feat: edit toast action and styling

--- src/exampleComponents/ToastDemo.tsx
+++ src/exampleComponents/ToastDemo.tsx
@@ -5,20 +5,45 @@ import { useToast } from '@/src/ui/use-toast';
 const ToastDemo = () => {
   const { toast } = useToast();
   return (
-    <Button
-      variant="outline"
-      onClick={() => {
-        toast({
-          title: 'This is toast',
-          description: 'Friday, February 10, 2023 at 5:57 PM',
-          action: (
-            <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
-          ),
-        });
-      }}
-    >
-      Add to calendar
-    </Button>
+    <div className="flex gap-x-2">
+      <Button
+        variant="outline"
+        onClick={() => {
+          toast({
+            title: 'This is toast',
+            description: 'Friday, February 10, 2023 at 5:57 PM',
+          });
+        }}
+      >
+        Toast default
+      </Button>
+      <Button
+        variant="outline"
+        onClick={() => {
+          toast({
+            title: 'This is toast',
+            description: 'Friday, February 10, 2023 at 5:57 PM',
+            variant: 'destructive',
+          });
+        }}
+      >
+        Toast destructive
+      </Button>
+      <Button
+        variant="outline"
+        onClick={() => {
+          toast({
+            title: 'This is toast',
+            description: 'Friday, February 10, 2023 at 5:57 PM',
+            action: (
+              <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
+            ),
+          });
+        }}
+      >
+        Toast with action
+      </Button>
+    </div>
   );
 };
 
--- src/ui/toaster.tsx
+++ src/ui/toaster.tsx
@@ -13,20 +13,16 @@ const Toaster = () => {
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        );
-      })}
+      {toasts.map(({ id, title, description, action, variant, ...props }) => (
+        <Toast key={id} {...props} variant={variant ?? 'default'}>
+          <div className="grid gap-1">
+            {title && <ToastTitle>{title}</ToastTitle>}
+            {description && <ToastDescription>{description}</ToastDescription>}
+          </div>
+          {action}
+          <ToastClose />
+        </Toast>
+      ))}
       <ToastViewport />
     </ToastProvider>
   );
