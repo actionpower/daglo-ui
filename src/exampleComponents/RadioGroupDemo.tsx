3db72ab42f0b31f8d1f2b9a58d770035e25f754c
yongmin.jeon <yongmin.jeon@actionpower.kr>
2024-06-04 16:05:14 +0900

    feat: add radio group

---
+++ src/exampleComponents/RadioGroupDemo.tsx
@@ -0,0 +1,23 @@
+import { Label } from '@/src/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/src/ui/radio-group';
+
+const RadioGroupDemo = () => {
+  return (
+    <RadioGroup defaultValue="comfortable">
+      <div className="flex items-center space-x-2">
+        <RadioGroupItem value="default" id="r1" />
+        <Label htmlFor="r1">Default</Label>
+      </div>
+      <div className="flex items-center space-x-2">
+        <RadioGroupItem value="comfortable" id="r2" />
+        <Label htmlFor="r2">Comfortable</Label>
+      </div>
+      <div className="flex items-center space-x-2">
+        <RadioGroupItem value="compact" id="r3" />
+        <Label htmlFor="r3">Compact</Label>
+      </div>
+    </RadioGroup>
+  );
+};
+
+export default RadioGroupDemo;
