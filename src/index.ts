49fe687d25ae39c2117bfcf137dd010869066233
yongmin.jeon <yongmin.jeon@actionpower.kr>
2024-06-13 16:59:14 +0900

    feat: check build result

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,141 @@
-import * as components from '@/src/components';
-import * as hooks from '@/src/hooks';
-import * as utils from '@/src/utils';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/src/components/Accordion';
+import { Alert, AlertDescription, AlertTitle } from '@/src/components/Alert';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/src/components/AlertDialog';
+import Badge from '@/src/components/Badge';
+import Button from '@/src/components/Button';
+import Checkbox from '@/src/components/Checkbox';
+import ContentsLayout from '@/src/components/ContentsLayout';
+import DatePicker from '@/src/components/DatePicker';
+import DatePickerWithRange from '@/src/components/DatePickerWithRange';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/src/components/Dialog';
+import Input from '@/src/components/Input';
+import Label from '@/src/components/Label';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/src/components/Pagination';
+import { RadioGroup, RadioGroupItem } from '@/src/components/Radio-group';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/src/components/Select';
+import Sidebar from '@/src/components/Sidebar';
+import SidebarMenuItem from '@/src/components/SidebarMenuItem';
+import SonnerToaster from '@/src/components/Sonner';
+import Switch from '@/src/components/Switch';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@/src/components/Tabs';
+import Textarea from '@/src/components/Textarea';
+import {
+  Toast,
+  ToastAction,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from '@/src/components/Toast';
+import Toaster from '@/src/components/Toaster';
 
-export { hooks, components, utils };
+export { useToast } from './hooks';
+export { sonnerToast, cn } from './utils';
+
+export {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+  Alert,
+  AlertDescription,
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  AlertTitle,
+  Badge,
+  Button,
+  Checkbox,
+  ContentsLayout,
+  DatePicker,
+  DatePickerWithRange,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  Input,
+  Label,
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+  RadioGroup,
+  RadioGroupItem,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Sidebar,
+  SidebarMenuItem,
+  SonnerToaster,
+  Switch,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+  Textarea,
+  Toast,
+  ToastAction,
+  ToastClose,
+  ToastDescription,
+  Toaster,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+};
