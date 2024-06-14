9a84cf4817efcfa3c747ff51d642a385ffe821ef
yongmin.jeon <yongmin.jeon@actionpower.kr>
2024-06-14 17:41:17 +0900

    feat: fix badge style

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/src/utils';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center rounded-[0.9375rem] border px-3 py-0.5 !text-14-r font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       variant: {
@@ -13,8 +13,9 @@ const badgeVariants = cva(
         secondary:
           'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
         destructive:
-          'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
+          'border-transparent bg-red-500 text-destructive-foreground shadow hover:bg-red-500/80',
         outline: 'text-foreground',
+        warn: 'border-transparent bg-yellow-400 text-black',
       },
     },
     defaultVariants: {
